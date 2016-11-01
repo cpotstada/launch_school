8ef01263fcf81e13c58fd0cb3909e52ea3388267
Christoph Potstada <ogd998@gmail.com>
2016-11-01 11:19:11 +0000

    pseudo code

---
+++ 101/lesson_2/pseudo_code.rb
@@ -0,0 +1,29 @@
+=begin
+
+1. a method that returns the sum of two integers
+- ask the user for the first integer
+- save the answer as a variable number1
+- ask the user for the second integer
+- save the answer as a varbiable number2
+- convert number1 and number to into integers
+- add number1 and number2 and return the result
+
+2. a method that takes an array of strings, and returns a string that is all 
+those strings concatenated together
+- save each array element as a string variable
+- combine all string variables into a new string variable
+- return the new string variable
+
+3. a method that takes an array of integers, and returns a new array with every
+other element
+- set an index variable to equal 0
+- create an empty array
+- return the element at index 0 via the index variable
+- add the returned variable to the new array
+- increase the index variable by 2
+- return element, and add to new array and increase index variable (like above)
+- stop when the index variable is equal or bigger than the original array length
+- return the new array
+
+
+=end
